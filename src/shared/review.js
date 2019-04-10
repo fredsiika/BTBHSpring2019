192fb9329a1106dd77c7b5f712d54509a5ba0761
vdauverg <vicentedauvergne@yahoo.ie>
2019-04-09 22:49:21 -0700

    improving review history

--- src/shared/review.js
+++ src/shared/review.js
@@ -4,52 +4,47 @@ import {
     MDBRow,
     MDBCol,
     MDBIcon,
-    MDBCard,
     MDBInput
 } from "mdbreact";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  .icon {
-    margin-left: 10px;
-    color: #0099cc;
-  }
+padding-bottom: 60px;
 
-  .icon-red{
-    margin-left: 10px;
-    color: red;
-  }
+//   .icon {
+//     margin-left: 10px;
+//     color: #0099cc;
+//   }
+
+//   .icon-red{
+//     margin-left: 10px;
+//     color: red;
+//   }
 
   .review-restaurant {
-    padding-top: 15px;
+    margin-top: 15px;
+    padding-top: 10px;
+    padding-bottom: 10px;
     text-align: center;
-    font-size: 16px;
+    background-color: #dfdfdf;
+    font-weight: 400;
   }
 
-  .review-container {
+  .thumbs-type {
+    display: inline-block;
+    text-align: center;
+    width: 32%;
   }
 
-  .thumb-type {
-    text-align: right;
-    font-size: 12px;
-    background-color: red;
-    display: inline;
+  .review-text {
+    text-align: justify;
+    text-overflow: ellipsis;
   }
 
-  .thumbs-container {
-    display: inline;
-    margin: 15px;
-  }
-
-  .comment-containter {
-    display: inline;
-  }
-
-  .card {
-    background-color: white;
-    border: 2px solid #f3f3f3;
-    align-self: auto;
-    margin: 15px auto;
+  p {
+    height: 50px;
+    overflow: hidden;
+    whitespace: no-wrap;
   }
 
   .input {
@@ -72,6 +67,7 @@ const Wrapper = styled.div`
 
   hr {
     margin: 0;
+    padding-bottom: 10px;
   }
 `;
 
@@ -81,38 +77,32 @@ class Review extends Component {
             <Wrapper>
                 <MDBRow>
                     <MDBCol
-                        className="review-container offset-md-3"
+                        className="review-container offset-md-2"
                         size="12"
                         sm="12"
-                        md="6"
+                        md="8"
                     >
                         <h5 className="review-restaurant">Miss Sue's Chicken</h5>
                         <hr />
-                        <MDBRow className="test">
-                            <div className="thumbs-container">
-                                <h5 className="thumbs-type">
-                                    Food
-                                </h5>
-                                <h5 className="thumbs-type">
-                                    Staff
-                                </h5>
-                                <h5 className="thumbs-type">
-                                    Category
-                                </h5>
-                            </div>
-                            <div className="thumbs-container">
-                                <h5><MDBIcon className="icon-red" far icon="thumbs-up" /></h5>
-                                <h5><MDBIcon className="icon-red" far icon="thumbs-up" /></h5>
-                                <h5><MDBIcon className="icon-red" far icon="thumbs-up" /></h5>
-                            </div>
-                            {/* <div className="comment-container">
-                                <MDBInput type="textarea" label="Example label" outline />
-                            </div> */}
-                            <div className="comment">
+                        <MDBRow>
+                            <MDBCol size="12" className="review-text">
                                 <p>
-                                    
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut convallis maximus ligula molestie consectetur. Praesent quis vulputate nunc. Aenean efficitur metus lectus, et maximus nisl vestibulum non. Curabitur sit amet dolor in sem pharetra congue. Vivamus mollis euismod lobortis. Integer sit amet odio eu mi tempor lobortis in sed lacus. Sed ultrices scelerisque urna vel consectetur. Phasellus nisl neque, condimentum vel posuere ut, vehicula a lorem.
+                                    Pellentesque et risus felis. Nulla nisi lacus, facilisis vitae leo nec, facilisis rutrum enim. Suspendisse eu quam et mi malesuada venenatis. Nullam id ante nunc. Pellentesque finibus laoreet hendrerit. Fusce eu risus dictum, tempus enim ut, tempor est. Quisque vestibulum mauris sit amet magna laoreet euismod eget eget massa. Aenean justo sem, vestibulum sed dui ac, lobortis euismod augue. Donec vestibulum, purus ut commodo egestas, metus felis consectetur nisl, et porttitor nulla sem a risus. Nulla dignissim, urna nec pretium aliquam, turpis ante facilisis tortor, sed elementum lorem purus vel ante. Quisque aliquet consectetur dolor, eget congue erat consectetur id. Sed non facilisis est, sed elementum ipsum. Aliquam quis velit libero. Curabitur efficitur ex posuere arcu consectetur, ut dapibus ex ornare. Integer tellus lectus, tristique quis dignissim fringilla, commodo id sem. Morbi arcu lacus, tempor et augue non, sollicitudin varius orci.
                                 </p>
-                            </div>
+                            </MDBCol>
+                        </MDBRow>
+                        <MDBRow>
+                            <MDBCol size="12">
+                                <MDBRow className="thumbs-container">
+                                    <h5 className="thumbs-type">Food<MDBIcon className="icon-red" far icon="thumbs-up" /></h5>
+                                    <h5 className="thumbs-type">Staff<MDBIcon className="icon-red" far icon="thumbs-up" /></h5>
+                                    <h5 className="thumbs-type">Atmosphere<MDBIcon className="icon-red" far icon="thumbs-up" /></h5>
+                                </MDBRow>
+                                {/* <div className="comment-container">
+                                <MDBInput type="textarea" label="Example label" outline />
+                                </div> */}
+                            </MDBCol>
                         </MDBRow>
                     </MDBCol>
                 </MDBRow>
